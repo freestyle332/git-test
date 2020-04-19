3224ee919a1ceb5364dfd9baf96d09354d61d8f7
freestyle332 <freestyle332@hotmail.com>
2020-04-19 15:12:34 +0300

    assignment1 if statement capitalise function changed to lower

--- assignment1-if.py
+++ assignment1-if.py
@@ -1,5 +1,5 @@
 name = input("Username: ")
-if name.capitalize() == "David":
+if name.lower() == "david":
     print("Hi,", name,"\b! The password is: @12")
 else:
     print("Hi,", name,"\b! See you later.")
